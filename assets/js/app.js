5524ace5189780dcf16c938d893a0dc53342fd2f
tazocar <tankodani@gmail.com>
2017-12-13 20:07:11 -0300

    maqueteado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,26 @@
 $(document).ready(function(){
+  //
+  $('.chips-autocomplete').material_chip({
+    placeholder: "Filtrar Restaurantes",
+    secondaryPlaceholder: ' ',
+    autocompleteOptions: {
+      data: {
+        'Uva': null,
+        'Coco': null,
+        'Maní': null,
+        'Fresa': null,
+        'Pera': null,
+      },
+      limit: Infinity,
+      minLength: 1
+    }
+  });
 
 });
 
-// //splash de 2 segundos
-// $(function(){
-//    setTimeout(function() {
-//       $('#splash').fadeOut(500);
-//    }, 2000);
-// });
+//splash de 2 segundos
+$(function(){
+   setTimeout(function() {
+      $('#splash').fadeOut(500);
+   }, 2000);
+});
